370f66dc6fc47076984699f205c8e5860d046b24
Akash <akash.chaudhary1306@gmail.com>
2021-11-16 18:53:21 +0100

    GFP Checkpoint 2

---
+++ gesture_flocking/include/gesture_flocking/flocker.h
@@ -0,0 +1,53 @@
+#pragma once
+#ifndef FORMATION_H
+#define FORMATION_H
+
+#include <ros/ros.h>
+#include <ros/package.h>
+#include <nodelet/nodelet.h>
+
+#include <mrs_lib/param_loader.h>
+#include <mrs_lib/attitude_converter.h>
+#include <mrs_lib/geometry/cyclic.h>
+
+#include <mrs_msgs/ReferenceStampedSrv.h>
+
+#include <mrs_msgs/Float64Stamped.h>
+
+#include <nav_msgs/Odometry.h>
+
+#include <std_srvs/Trigger.h>
+
+#include <message_filters/subscriber.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
+
+#include <mrs_lib/batch_visualizer.h>
+#include <mrs_lib/attitude_converter.h>
+
+#include <string>
+
+struct proximalPar {
+  
+  double _desired_distance_;
+  double _range_multipler_;
+  double _steepness_potential_;
+  double _strength_potential_;
+
+  double max_range_;
+  double noise_;
+
+};
+
+struct motionControlPar{
+  
+  double _K1_;  // Linear gain
+  double _K2_;  // Angular gain
+  double _K3_;
+  
+  double _move_forward_;
+  double _interpolate_coeff_;
+  bool  _fixed_heading_;
+};
+
+#endif
---
+++ gesture_flocking/src/flocker.cpp
@@ -0,0 +1,28 @@
+#include<flocker.h>
+
+class FlockerClass
+{
+    public:
+        FlockerClass(ros::NodeHandle* nodehandle);
+
+    private:
+        proximalPar ProxParameters;
+        motionControlPar motionParameters;
+
+
+        double getProximalMagnitude(double range);
+
+        void neighborCallback();
+
+
+};
+
+
+double FlockerClass::getProximalMagnitude(double range)
+{
+    double proximalMag =    -4*ProxParameters._steepness_potential_ * ProxParameters._strength_potential_ / range*
+                            (2*pow(ProxParameters.noise_ / range, 2*ProxParameters._steepness_potential_) - 
+                            pow(ProxParameters.noise_ / range, ProxParameters._steepness_potential_));
+    return proximalMag;
+
+}
